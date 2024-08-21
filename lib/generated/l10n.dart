ed4e7180984bd524535fc65c8df0f6d0fa38c1dc
Eslam Mohammed Saad <eslamooo5522@outlook.com>
2024-08-21 15:27:42 +0300

    Edit project file structure

---
+++ lib/generated/l10n.dart
@@ -0,0 +1,309 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(_current != null,
+        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+
+      return instance;
+    });
+  }
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(instance != null,
+        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `Marché`
+  String get title {
+    return Intl.message(
+      'Marché',
+      name: 'title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Home`
+  String get home {
+    return Intl.message(
+      'Home',
+      name: 'home',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Category`
+  String get category {
+    return Intl.message(
+      'Category',
+      name: 'category',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Cart`
+  String get cart {
+    return Intl.message(
+      'Cart',
+      name: 'cart',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Favorite`
+  String get favorite {
+    return Intl.message(
+      'Favorite',
+      name: 'favorite',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Best Selling Products`
+  String get bestSellingProducts {
+    return Intl.message(
+      'Best Selling Products',
+      name: 'bestSellingProducts',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Account`
+  String get account {
+    return Intl.message(
+      'Account',
+      name: 'account',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Contact us`
+  String get contact {
+    return Intl.message(
+      'Contact us',
+      name: 'contact',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `About us`
+  String get about {
+    return Intl.message(
+      'About us',
+      name: 'about',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sign out`
+  String get signOut {
+    return Intl.message(
+      'Sign out',
+      name: 'signOut',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Hi`
+  String get hi {
+    return Intl.message(
+      'Hi',
+      name: 'hi',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Order Now`
+  String get order {
+    return Intl.message(
+      'Order Now',
+      name: 'order',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Total`
+  String get total {
+    return Intl.message(
+      'Total',
+      name: 'total',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Clothes`
+  String get clothes {
+    return Intl.message(
+      'Clothes',
+      name: 'clothes',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Electronics`
+  String get electronics {
+    return Intl.message(
+      'Electronics',
+      name: 'electronics',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Beauty`
+  String get beauty {
+    return Intl.message(
+      'Beauty',
+      name: 'beauty',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sports`
+  String get sports {
+    return Intl.message(
+      'Sports',
+      name: 'sports',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Toys`
+  String get Toys {
+    return Intl.message(
+      'Toys',
+      name: 'Toys',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Books`
+  String get books {
+    return Intl.message(
+      'Books',
+      name: 'books',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Grocery`
+  String get grocery {
+    return Intl.message(
+      'Grocery',
+      name: 'grocery',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Item removed from cart`
+  String get itemremoved {
+    return Intl.message(
+      'Item removed from cart',
+      name: 'itemremoved',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Item added to cart`
+  String get itemadded {
+    return Intl.message(
+      'Item added to cart',
+      name: 'itemadded',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Language`
+  String get applang {
+    return Intl.message(
+      'Language',
+      name: 'applang',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'ar'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
